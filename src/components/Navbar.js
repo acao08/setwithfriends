5f6b01f0401afc287290aa97d57d2b7ac2bcfdb5
acao08 <147471817+acao08@users.noreply.github.com>
2025-05-10 21:28:45 -0700

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -147,7 +147,7 @@ function Navbar() {
               <Link
                 target="_blank"
                 rel="noopener"
-                href={`https://github.com/eltoder/setwithfriends/releases/tag/v${version}`}
+                href={`https://github.com/acao08/setwithfriends/releases/tag/v${version}`}
               >
                 Release Notes
               </Link>
